02122148baef7c1d26f8659af8ca66da590fa172
황인재 <hij2696@hanyang.ac.kr>
2023-09-20 07:12:19 +0900

    modified update and delete

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
-
+const methodOverride = require('method-override');
 const config = require("./config/key.js");
+
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
+
+app.use('/public', express.static('public'));
+// app.use(express.static(__dirname + '/public'));
 
 var db;
 MongoClient.connect(config.mongoURI, { useUnifiedTopology: true }, function (err, client) {
@@ -17,11 +22,11 @@ MongoClient.connect(config.mongoURI, { useUnifiedTopology: true }, function (err
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.render('index.ejs');
 });
 
 app.get('/write', (req, res) => { 
-  res.sendFile(__dirname + '/write.html')
+  res.render('write.ejs');
 });
 
 app.post('/add', (req, res) => {
@@ -50,5 +55,26 @@ app.delete('/delete', (req, res) => {
   db.collection('post').deleteOne(req.body, (err, result) => {
     console.log('deletion Successful');
     res.status(200).send({ message : 'success!' });
+  });
+});
+
+app.get('/detail/:id', (req, res) => {
+  db.collection('post').findOne({ _id : parseInt(req.params.id) }, (err, result) => {
+    console.log(result);
+    res.render('detail.ejs', { data : result });
+  });
+});
+
+app.get('/edit/:id', (req, res) => {
+  db.collection('post').findOne({ _id : parseInt(req.params.id) }, (err, result) => {
+    console.log(result);
+    res.render('edit.ejs', { post : result });
+  });
+});
+
+app.put('/edit', (req, res) => {
+  db.collection('post').updateOne({ _id : parseInt(req.body.id) }, { $set : { title : req.body.title, date : req.body.date } }, (err, result) => {
+    console.log('Successfully modify');
+    res.redirect('/list');
   });
 });
